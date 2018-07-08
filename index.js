1f6aebcbbbacacf98c51fc951a16c63c21965b83
Joshua Olson <0joshuaolson1@pm.me>
2018-07-07 23:29:19 -0500

    fix syntax error, add bigint comments

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 // var exports;
 exports = {
-  //doesn't support N = 0
-  solveAXBN: function(a,b,n,f=,B=n,t=0,T=1,_){//f= not finished...
-    for(;b;B=t,t=_){
-      _=T
-      T=t-Math.floor(B/b)*T
-      t=b
-      b=B%b
+  // a,b > 0; b < n; modified from Wikipedia
+  solveAXBN: function(a,b,n/*,divmod,...*/,B,t,T,_){
+    /*
+    divmod=divmod||...
+    ...
+    */
+    for(B=n,t=0,T=1;b;B=t,t=_){
+      _=T;
+      T=t-Math.floor(B/b)*T;
+      t=b;
+      b=B%b;
     }
-    return(_>0?_:_+n)*a%n
+    return(_>0?_:_+n)*a%n;
   },
 
-  fromResidues: function(a,p,n,m=1,i=0){
-    for(n=a[i=0];++i<p.length;){
-      m*=p[i-1]
-      n+=m*f(p[i]+a[i]-n%p[i],m,p[i])
+  fromResidues: function(a,p/*,muladd,divmod,...*/,m,n,i){
+    /*
+    muladd=muladd||...
+    ...
+    */
+    for(m=1,n=a[i=0];++i<p.length;){
+      m*=p[i-1];
+      n+=m*this.solveAXBN(p[i]+a[i]-n%p[i],m,p[i]/*,divmod,...*/);
     }
-    return n
+    return n;
   },
 
   test: function(){
